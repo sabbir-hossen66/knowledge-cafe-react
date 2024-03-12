c92e7ff541423923cc310f4174d18020b776bcff
Sabbir Hossen <sabbir.ms2000@gmail.com>
2024-03-13 03:09:04 +0600

    blog section is running

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,13 +1,25 @@
 import PropTypes from 'prop-types';
 
 const Blog = ({ blog }) => {
-  console.log(blog);
+  const { title, cover, author, author_img, reading_time } = blog
   return (
-    <div>
-
+    <div className='border-2 p-5 rounded-md'>
+      <img src={cover} alt={`cover picture of title ${title}`} />
+      <div className='flex justify-between my-2'>
+        <div>
+          <img className='w-16' src={author_img} alt="" />
+        </div>
+        <div>
+          <p>{reading_time}</p>
+        </div>
+      </div>
+      <p className='font-semibold'>title:{title}</p>
     </div>
   );
 };
+
+
+
 Blog.propTypes = {
   blog: PropTypes.object.isRequired
 }
--- src/components/Header/Blogs/Blogs.jsx
+++ src/components/Header/Blogs/Blogs.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../../Blog/Blog";
 
 
 const Blogs = () => {
@@ -14,6 +15,16 @@ const Blogs = () => {
   return (
     <div className="md:w-2/3">
       <h1>Blogs</h1>
+      <div className="">
+
+        {
+          blogs.map(blog => <Blog
+            key={blog.id}
+            blog={blog}>
+
+          </Blog>)
+        }
+      </div>
     </div>
   );
 };
