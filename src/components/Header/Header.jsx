51c0e5b1f1f1b03285fbed377e7ab446721559fa
Sabbir Hossen <sabbir.ms2000@gmail.com>
2024-03-13 01:44:31 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import profile from '../../assets/images/profile.png'
+
+const Header = () => {
+  return (
+    <div className='flex justify-between mx-4 items-center mt-4'>
+      <h1 className='text-4xl font-bold'>knowledge</h1>
+      <img src={profile} alt="" />
+    </div>
+  );
+};
+
+export default Header;
